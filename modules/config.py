ae14e06e649eec1c18ea86785830b6054a3a8d55
HARSH OP <123957402+Musicharshi@users.noreply.github.com>
2023-01-31 00:24:01 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -8,19 +8,31 @@ que = {}
 admins = {}
 
 API_ID = int(getenv("API_ID"))
+
 API_HASH = getenv("API_HASH")
+
 BOT_TOKEN = getenv("BOT_TOKEN")
+
 MONGO_DB_URI = getenv("MONGO_DB_URI")
+
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","))
+
 DURATION_LIMIT = int(getenv("DURATION_LIMIT","300"))
+
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME",)
+
 OWNER_ID = list(map(int, getenv("OWNER_ID",)
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",https://github.com/Musicharshi/musicharshi")
+
 START_IMG_URL = getenv(
     "START_IMG_URL", )
+
 STRING_SESSION = getenv("STRING_SESSION")
+
 OWNER_USERNAME = getenv("OWNER_USERNAME")
+
 SUPPORT_GROUP = getenv("SUPPORT_GROUP")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL")
 
