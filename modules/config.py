90d49fdf707a8fdf51e789b3cee891ff3ba9224d
HARSH OP <123957402+Musicharshi@users.noreply.github.com>
2023-01-31 02:13:46 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,28 +7,16 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID","15418866"))
-
-API_HASH = getenv("API_HASH","dbbf679a4b429fab1bfea0b52b8f9be8")
-
-BOT_TOKEN = getenv("BOT_TOKEN", "5850385258:AAGgtswykyvc9OO9i1hHlECPRMmTvtBruDI")
-
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://vcbot:vcbot@cluster0.yqipgxg.mongodb.net/?retryWrites=true&w=majority")
-
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1001618068091"))
-
-DURATION_LIMIT = int(getenv("DURATION_LIMIT","300"))
-
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","harsh")
-
-OWNER_ID = list(map(int, getenv("OWNER_ID","5897579715")
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO","https://github.com/Musicharshi/musicharshi")
-
-START_IMG_URL = getenv(
-    "START_IMG_URL", )
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
+STRING_SESSION = getenv("STRING_SESSION")
+OWNER_USERNAME = getenv("OWNER_USERNAME")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL")
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5897579715").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
 aiohttpsession = aiohttp.ClientSession()
+
